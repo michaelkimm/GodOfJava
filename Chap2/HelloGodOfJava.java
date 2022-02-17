e3d0fd2ecd933201d56666b0969d40cc301e72d6
michael <mskim_451@dongguk.edu>
2022-02-17 11:14:11 +0900

    GodOfJava

---
+++ Chap2/HelloGodOfJava.java
@@ -0,0 +1,9 @@
+public class HelloGodOfJava
+{
+  public static void main(String[] args)
+  {
+    System.out.print("Hello God of Java!!!");
+    System.out.print("Hello God of Java!!!");
+  }
+}
+
---
+++ Chap2/Profile.java
@@ -0,0 +1,8 @@
+public class Profile
+{
+  public static void main(String[] args)
+  {
+    System.out.println("My name is Minsuk");
+    System.out.println("My age is 28");
+  }
+}
